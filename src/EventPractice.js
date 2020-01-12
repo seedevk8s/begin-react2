2f42f89995251e35c1a37bcf9eb0c0422e916872
chjin <seedevk8s@gmail.com>
2020-01-12 17:07:23 +0900

    state에 텍스트를 잘 담음.

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const EventPractice = () => {
+    const [form, setForm] = useState({
+        message: ''
+    });
+    const { message } = form;
+
+    const onChange = e => {
+        const nextForm = {
+            ...form,  // 기존의 form 내용을 이 자리에 복사 한 뒤
+            [e.target.name]: e.target.value  // 원하는 값을 덮어씌우기
+        };
+        setForm(nextForm);      //state에 텍스트를 잘 담음.
+    };
+
     return (
         <div>
             <h1>이벤트 연습!!</h1>
@@ -9,11 +22,7 @@ const EventPractice = () => {
                 type="text"
                 name="username"
                 placeholder="유저명"
-                onChange={
-                    (e) => {
-                        console.log(e.target.value);    {/* e : 이벤트 객체, e.target.value : 변할 인풋값 */}
-                    }
-                }
+                onChange={onChange}
             />
         </div>
     );
